f776726bc2d3ab18ebb655586c0cbc5f815250dc
Vijayakumar <msvijayakumar1>
2022-12-22 05:58:03 -0800

    library

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Navbar from './layout/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import AddBook from './books/AddBook';
+import EditBook from './books/EditBook';
+import ViewBook from './books/ViewBook';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+
+        <Routes>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/about" component={About}></Route>
+          <Route exact path="/contact" component={Contact}></Route>
+          <Route exact path="/books/add" component={AddBook} />
+          <Route exact path="/books/edit/:id" component={EditBook} />
+          <Route exact path="/books/:id" component={ViewBook} />
+          <Route component={NotFound}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const About = () => {
+    return (
+      <div className="container">
+        <div className="py-4">
+          <h1>About Us Page</h1>
+         
+          <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At veritatis
+            earum sapiente debitis repudiandae, modi voluptas adipisci ad in
+            dolores minima soluta deserunt hic? Quas similique nulla quam enim
+            eveniet.
+          </p>
+          <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At verita//tis
+            earum sapiente debitis repudiandae, modi voluptas adipisci ad in
+            dolores minima soluta deserunt hic? Quas similique nulla quam enim
+            eveniet.
+          </p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default About;
+  
